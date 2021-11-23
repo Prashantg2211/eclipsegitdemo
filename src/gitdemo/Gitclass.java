f9267b8a5af3e92e4d5ac046497f0830ecf88859
prashant <prashanrgrj515@gmail.com>
2021-11-23 15:22:29 +0530

    createsd  afile at staging area.[.....

---
+++ src/gitdemo/Gitclass.java
@@ -0,0 +1,22 @@
+package gitdemo;
+
+
+
+public class Gitclass{
+
+	public static void main(String args[]) {
+		System.out.println("\n\t\t Stack Operations using array");
+		System.out.println("\n\t 1. Push");
+		System.out.println("\n\t 2. Pop");
+		System.out.println("\n\t 3. Peek");
+		System.out.println("\n\t 4. Display");
+		System.out.println("\n\t 5. Sort Array");
+		System.out.println("\n\t 6. Search element");
+		System.out.println("\n\t 7. Exit");
+		System.out.println("\n Please enter your choice (1-7)");
+		
+		
+		System.out.println("\n\t 1. P");
+
+	}
+}
